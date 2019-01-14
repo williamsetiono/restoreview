cf46d31e21a9bec07dce0c22be60a29b3b5af4d5
William Setiono <williamsetiono@williams-macbook-air.local>
2019-01-14 10:21:40 +0700

    Initial Commit

---
+++ FoodTaskerMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by William Setiono on 14/01/19.
+//  Copyright © 2019 William Setiono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
